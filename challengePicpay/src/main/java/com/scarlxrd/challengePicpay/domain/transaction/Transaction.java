8d5b17820f4d859ba5bd5710d5fdf5f0cb5ad761
Guilherme Santos <guilhermesantos2004gss@gmail.com>
2024-01-13 19:43:31 -0300

    feat: create transaction entity

---
+++ challengePicpay/src/main/java/com/scarlxrd/challengePicpay/domain/transaction/Transaction.java
@@ -0,0 +1,31 @@
+package com.scarlxrd.challengePicpay.domain.transaction;
+
+import com.scarlxrd.challengePicpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transctions")
+@Table(name = "transctions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
