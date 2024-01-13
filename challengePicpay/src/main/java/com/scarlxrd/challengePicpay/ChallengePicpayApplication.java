062f8b0366d7d507132570ca89f7e61513483033
Guilherme Santos <guilhermesantos2004gss@gmail.com>
2024-01-13 19:41:48 -0300

    initial commit

---
+++ challengePicpay/src/main/java/com/scarlxrd/challengePicpay/ChallengePicpayApplication.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.challengePicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengePicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengePicpayApplication.class, args);
+	}
+
+}
---
+++ challengePicpay/src/test/java/com/scarlxrd/challengePicpay/ChallengePicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarlxrd.challengePicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengePicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
