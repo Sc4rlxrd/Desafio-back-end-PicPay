0bb5b8763774c88dc2d75dea3d7c76ca2e972a96
Guilherme Santos <guilhermesantos2004gss@gmail.com>
2024-01-14 16:15:34 -0300

    feat: infra config

---
+++ challengePicpay/src/main/java/com/scarlxrd/challengePicpay/infra/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.scarlxrd.challengePicpay.infra;
+
+import com.scarlxrd.challengePicpay.dtos.ExceptionDto;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception){
+        ExceptionDto exceptionDto = new ExceptionDto("Usuário já cadastrado.","400");
+        return ResponseEntity.badRequest().body(exceptionDto);
+    }
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception){
+        ExceptionDto exceptionDto = new ExceptionDto(exception.getMessage(),"500");
+        return ResponseEntity.internalServerError().body(exceptionDto);
+    }
+
+
+}
